6a4b3799e4d8c1a4e88d8ff804a804c1a469c174
Sokol Saiti <sokol@saiti.io>
2022-12-03 09:46:51 +0100

    Step 2 of Day 2 doesn't work. Need to fix it.

--- lib/day2.ex
+++ lib/day2.ex
@@ -11,17 +11,40 @@ defmodule Aoc2022.Day2 do
   def process_input do
     {:ok, content} = File.read("./input/day2.txt")
 
-    guides =
+    step1_result =
       content
       |> String.split("\r\n")
       |> Enum.map(fn x ->
-        translate(x)
+        translate(x, :all)
       end)
       |> Enum.map(fn x -> %{x | result: who_won?(x)} end)
       |> Enum.map(fn %{:m => _, :o => _, :result => r, :shape_value => v} -> r + v end)
       |> Enum.sum()
 
-    guides
+    step2_result =
+      content
+      |> String.split("\r\n")
+      |> Enum.map(fn x ->
+        translate(x, :oponent_only)
+      end)
+      |> Enum.map(fn x ->
+        {:ok, oponent_shape} = Map.fetch(x, :o)
+        {:ok, my_instruction} = Map.fetch(x, :m)
+        IO.inspect(x)
+        y = %{x | m: transform_my_shape(oponent_shape, my_instruction)}
+        IO.inspect(y)
+        %{y | shape_value: get_value(transform_my_shape(oponent_shape, my_instruction))}
+      end)
+      |> Enum.map(fn x -> %{x | result: who_won?(x)} end)
+      |> IO.inspect()
+      |> Enum.map(fn %{:m => _, :o => _, :result => r, :shape_value => v} -> r + v end)
+      |> Enum.filter(fn %{:m => me, :o => oponent, :result => r, :shape_value => v} ->
+        me == oponent
+      end)
+
+    # |> Enum.sum()
+
+    step2_result
   end
 
   def who_won?(%{m: :scissors, o: :paper}) do
@@ -44,15 +67,69 @@ defmodule Aoc2022.Day2 do
     0
   end
 
-  def translate(guide) do
-    [first, second] = String.split(guide, " ")
+  def translate(guide, :all) do
+    [oponent, me] = String.split(guide, " ")
 
     %{
-      o: translate_input(first),
-      m: translate_input(second),
+      o: translate_input(oponent),
+      m: translate_input(me),
       result: 0,
-      shape_value: get_value(translate_input(second))
+      shape_value: get_value(translate_input(me)),
+      original_shape: translate_input(me),
+      instruction: me
     }
+  end
+
+  def translate(guide, :oponent_only) do
+    [oponent, me] = String.split(guide, " ")
+
+    %{
+      o: translate_input(oponent),
+      m: me,
+      result: 0,
+      shape_value: get_value(translate_input(me)),
+      original_shape: translate_input(me),
+      instruction: me
+    }
+  end
+
+  # draw
+  def transform_my_shape(oponent, "Y") do
+    oponent
+  end
+
+  # loose
+  def transform_my_shape(oponent, "X") do
+    lose(oponent)
+  end
+
+  # win
+  def transform_my_shape(oponent, "Z") do
+    win(oponent)
+  end
+
+  defp win(:paper) do
+    :scissors
+  end
+
+  defp win(:rock) do
+    :paper
+  end
+
+  defp win(:scissors) do
+    :rock
+  end
+
+  defp lose(:paper) do
+    :scissors
+  end
+
+  defp lose(:rock) do
+    :paper
+  end
+
+  defp lose(:scissors) do
+    :rock
   end
 
   defp get_value(:rock) do
