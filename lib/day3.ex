cf3cac29cbe8ace0c829e6d716d9af20da9c68e0
Sokol Saiti <sokol@saiti.io>
2022-12-05 09:32:55 +0100

    Day 3 part 2 done

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,10 +1,10 @@
-defmodule Aoc_2022.Day3 do
+defmodule Aoc2022.Day3 do
   def process_input do
     {:ok, content} = File.read("./input/day3.txt")
 
-    rucksacks =
+    items =
       content
-      |> String.split("\n")
+      |> String.split("\r\n")
       |> Enum.map(fn x ->
         String.split_at(x, div(String.length(x), 2))
       end)
@@ -21,6 +21,24 @@ defmodule Aoc_2022.Day3 do
       end)
       |> Enum.sum()
 
-    rucksacks
+    common_items =
+      content
+      |> String.split("\r\n")
+      |> Enum.chunk_every(3)
+      |> Enum.map(fn [a, b, c] ->
+        MapSet.intersection(MapSet.new(String.graphemes(a)), MapSet.new(String.graphemes(b)))
+        |> MapSet.intersection(MapSet.new(String.graphemes(c)))
+        |> MapSet.to_list()
+      end)
+      |> List.flatten()
+      |> Enum.map(fn x ->
+        Enum.find_index(
+          String.graphemes("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+          fn y -> y == x end
+        ) + 1
+      end)
+      |> Enum.sum()
+
+    {items, common_items}
   end
 end
--- test/aoc2022_test.exs
+++ test/aoc2022_test.exs
@@ -2,6 +2,7 @@ defmodule Aoc2022Test do
   use ExUnit.Case
   alias Aoc2022.Day2
   alias Aoc2022.Day1
+  alias Aoc2022.Day3
 
   test "Day 1 - Step 1" do
     %{max: m, top3: _} = Day1.process_input()
@@ -17,5 +18,11 @@ defmodule Aoc2022Test do
     {step1, step2} = Day2.process_input()
     assert step1 == 13484
     assert step2 == 13433
+  end
+
+  test "Day 3" do
+    {step1, step2} = Day3.process_input()
+    assert step1 == 7850
+    assert step2 == 2581
   end
 end
