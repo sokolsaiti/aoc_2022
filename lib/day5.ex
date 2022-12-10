1741f391483bd17785b9af65619ba1e0af967f5b
Sokol Saiti <sokol@saiti.io>
2022-12-10 22:32:25 +0100

    Day 5 Part 1 Done

--- lib/day5.ex
+++ lib/day5.ex
@@ -6,20 +6,11 @@ defmodule Aoc2022.Day5 do
       content
       |> String.split("\r\n\r\n")
 
-    {buckets_row, stacks} =
+    {_, stacks} =
       initial_state_content
       |> String.split("\r\n")
       |> Enum.reverse()
       |> List.pop_at(0)
-
-    parsed_instructions =
-      instructions
-      |> String.split("\r\n")
-
-    stack_buckets =
-      buckets_row
-      |> String.split(" ", trim: true)
-      |> Map.new(fn x -> {String.to_integer(x), []} end)
 
     parsed_stacks =
       stacks
@@ -30,12 +21,45 @@ defmodule Aoc2022.Day5 do
       |> Enum.map(fn x -> Tuple.to_list(x) |> List.delete("") end)
       |> Enum.map(fn x -> x |> Enum.filter(fn y -> y != "" end) end)
 
-    parsed_instructions
+    parsed_instructions =
+      instructions
+      |> String.split("\r\n")
+      |> Enum.map(&parse_instruction/1)
+
+    move_n(parsed_instructions, parsed_stacks)
   end
 
-  def parse_row("", acc) do
-    Enum.reverse(acc)
-    # acc
+  defp pick_stack(list_of_stacks, position) do
+    {stack, _} = List.pop_at(list_of_stacks, position)
+    stack
+  end
+
+  def move_n([], stacks) do
+    stacks
+  end
+
+  def move_n(instructions, stacks) do
+    {{count, from, to}, rest_of_instructions} = List.pop_at(instructions, 0)
+
+    src = pick_stack(stacks, from - 1)
+
+    dst = pick_stack(stacks, to - 1)
+
+    {new_src, new_dst} = move_box(src, dst, count)
+    inter_result = List.replace_at(stacks, to - 1, new_dst)
+    result = List.replace_at(inter_result, from - 1, new_src)
+
+    move_n(rest_of_instructions, result)
+  end
+
+  def move_box(from, to, 0) do
+    {from, to}
+  end
+
+  def move_box(from, to, count) when count > 0 do
+    {box, rest} = List.pop_at(from, -1)
+    new = List.insert_at(to, -1, box)
+    move_box(rest, new, count - 1)
   end
 
   def parse_instruction(instruction) do
@@ -45,19 +69,16 @@ defmodule Aoc2022.Day5 do
     {String.to_integer(a), String.to_integer(f), String.to_integer(t)}
   end
 
+  def parse_row("", acc) do
+    Enum.reverse(acc)
+    # acc
+  end
+
   def parse_row(row, acc) do
     {col, rest} = String.split_at(row, 4)
     acc = add_2_list(String.trim(col), acc)
 
     parse_row(rest, acc)
-  end
-
-  def move_out(list) do
-    List.pop_at(list, -1)
-  end
-
-  def move_in(list, box) do
-    List.insert_at(list, -1, box)
   end
 
   defp add_2_list(item, list) do
